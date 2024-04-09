4613d0806f35594377b1acfa8a76b10794e6d153
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2024-04-09 23:23:43 +0530

    Update Test1.js

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -3,7 +3,10 @@ describe('First Test Suite', function(){
     it('First Case', function(){
         cy.visit('https://rahulshettyacademy.com/seleniumPractise');
         cy.get('input.search-keyword').type("ca");
-        cy.get('.product').should('have.length',4);
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length',4);
+        cy.get('.products').find('.product').should('have.length',4);
+        cy.get('.products').find('.product').eq(2).contains("ADD TO CART").click();
         expect(true).to.equals(true);
     })
 
