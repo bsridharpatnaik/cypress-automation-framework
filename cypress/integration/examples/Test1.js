3ffc961adfa4180db29011229d92f9bfd7654b92
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2024-04-09 20:35:19 +0530

    First Test

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,7 @@
+describe('First Test Suite', function(){
+    it('First Case', function(){
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise');
+        expect(true).to.equals(true);
+    })
+
+})
