3058803af85eb095562617453df416c77368b56e
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2024-04-09 22:31:28 +0530

    Basic assertion

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,6 +1,9 @@
+/// <reference types="cypress"/>
 describe('First Test Suite', function(){
     it('First Case', function(){
         cy.visit('https://rahulshettyacademy.com/seleniumPractise');
+        cy.get('input.search-keyword').type("ca");
+        cy.get('.product').should('have.length',4);
         expect(true).to.equals(true);
     })
 
